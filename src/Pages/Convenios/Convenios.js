5dfd8aa5e56d1c4131dc74c6d760c8713e5ceab8
Josué Pérez <jperarm@gmail.com>
2022-01-29 16:45:27 +0000

    carga inicial

---
+++ src/Pages/Convenios/Convenios.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BasicLayout from '../../Layout/BasicLayout';
+import "./Convenios.scss";
+
+export default function Convenios() {
+  return (
+    <BasicLayout className="home">
+    <h2>Estamos mostrando el listado de convenios</h2>
+</BasicLayout>
+    );
+}
